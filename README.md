1c50da15c842d8dc39b0d80b779205fa4761feae
Elena Litvyak <ekologi4ka88@gmail.com>
2023-11-10 02:54:54 +0300

    createCard and deleteCard

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
-
+Проект "Mesto'
+https://github.com/ElenaLitvyak/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,33 @@
+const content = document.querySelector('.content');
+const cardContainer = content.querySelector('.places__list');
+
+function createCard(cardData) {
+  const cardTemplate = document.querySelector('#card-template').content;
+   
+  initialCards.forEach(function(cardData) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+
+    addCard(cardElement);
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function(event) {
+      let item = event.target.closest('.card');
+      item.remove();
+    });   
+  });    
+}
+
+function addCard(cardElement) {
+  cardContainer.append(cardElement);
+}
+
+const addButton = content.querySelector('.profile__add-button');
+addButton.addEventListener('click', createCard);
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
