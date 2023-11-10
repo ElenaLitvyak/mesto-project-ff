8e214ad682d6e946441366b2d87d1cb796f7fb0a
Elena Litvyak <ekologi4ka88@gmail.com>
2023-11-10 13:51:26 +0300

    separation function. add alt

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 const content = document.querySelector('.content');
 const cardContainer = content.querySelector('.places__list');
 
-function createCard(cardData) {
+function createCard(cardData, handleDeleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
-   
-  initialCards.forEach(function(cardData) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__title').textContent = cardData.name;
-    cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
 
-    addCard(cardElement);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', handleDeleteCard);
 
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', function(event) {
-      let item = event.target.closest('.card');
-      item.remove();
-    });   
-  });    
+  return cardElement;
 }
 
-function addCard(cardElement) {
+function renderCard(cardElement) {
   cardContainer.append(cardElement);
 }
 
-const addButton = content.querySelector('.profile__add-button');
-addButton.addEventListener('click', createCard);
+function handleDeleteCard(event) {
+  const item = event.target.closest('.card');
+  item.remove();
+}
 
+initialCards.forEach((card) => { 
+  renderCard(createCard(card, handleDeleteCard))
+});  
 
 // @todo: Темплейт карточки
 
